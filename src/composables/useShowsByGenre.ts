00ac09f9d3b4816d17522c6df7b02c5a84899487
Dzmitry Viartsinski <dviartsinski.external@epo.org>
2026-03-11 15:26:28 +0100

    add filter by genre

--- src/composables/useShowsByGenre.ts
+++ src/composables/useShowsByGenre.ts
@@ -1,21 +1,29 @@
-import { computed } from 'vue';
+import { computed, type Ref } from 'vue';
 import { useDashboardShowsStore } from '../stores/useDashboardShowsStore';
 import type { Show } from '../types/entities/Show';
 
 type ShowsByGenre = Record<string, Show[]>;
 
-export const useShowsByGenre = () => {
+export const useShowsByGenre = (filterGenre: Ref<string>) => {
   const showsStore = useDashboardShowsStore();
+
+  const allGenres = computed(() => {
+    return [
+      ...new Set<string>(showsStore.shows.flatMap((show) => show.genres)),
+    ];
+  });
 
   const showsByGenre = computed(() => {
     return showsStore.shows.reduce<ShowsByGenre>((acc, show) => {
       show.genres.forEach((genre) => {
-        if (!acc[genre]) acc[genre] = [];
-        acc[genre].push(show);
+        if (filterGenre.value === 'all' || genre === filterGenre.value) {
+          if (!acc[genre]) acc[genre] = [];
+          acc[genre].push(show);
+        }
       });
       return acc;
     }, {});
   });
 
-  return { showsByGenre };
+  return { showsByGenre, allGenres };
 };
--- src/screens/Dashboard.vue
+++ src/screens/Dashboard.vue
@@ -5,14 +5,23 @@ import ShowsSection from '../components/ShowsSection.vue';
 import Loader from '../components/Loader.vue';
 import Error from '../components/Error.vue';
 
+import { ref } from 'vue';
+const selected = ref<string | null>('all');
+
 const { isLoading, error } = useLoadDashboardShows();
-const { showsByGenre } = useShowsByGenre();
+const { showsByGenre, allGenres } = useShowsByGenre(selected);
 </script>
 
 <template>
   <ContentWrap>
     <Error v-if="error" :message="error" />
     <Loader v-if="isLoading" class="mt-40" />
+    <select v-model="selected">
+      <option value="all">Please select one</option>
+      <option v-for="genre in allGenres" :key="genre" :value="genre">
+        {{ genre }}
+      </option>
+    </select>
     <div
       v-if="!isLoading && !error"
       v-for="(shows, genre) in showsByGenre"
